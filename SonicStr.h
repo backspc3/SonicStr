c535c092d5b3499615bdaa077ca69a8f23fec8d5
backspc3 <juansalvadoraviles@gmail.com>
2024-08-06 18:24:36 +0200

    Added SIMD and SWAR version of substring search based on amazing article I found...

--- SonicStr.h
+++ SonicStr.h
@@ -2,16 +2,18 @@
 #define SONICSTR_H
 
 #include <immintrin.h>
-#include <iostream>
+#include <cstring>
+#include <bit>
 
 #define SONICSTR_INLINE     inline
 #define SONICSTR_NOEXCEPT   noexcept
+#define SONICSTR_CONSTEXPR  constexpr
 
 namespace Sonic
 {
 
 // Internal comparison function which tries to use SIMD operations for string comparison.
-static SONICSTR_INLINE bool simd_str_cmp( const char* aStr, const char* bStr, size_t aLen ) SONICSTR_NOEXCEPT
+static SONICSTR_INLINE SONICSTR_CONSTEXPR bool simd_str_cmp( const char* aStr, const char* bStr, size_t aLen ) SONICSTR_NOEXCEPT
 {
 #ifdef __AVX2__
     // 32 byte blocks.
@@ -40,11 +42,11 @@ static SONICSTR_INLINE bool simd_str_cmp( const char* aStr, const char* bStr, si
         // value must be 0xffffffff.
 
         // Load 32 chars of both _A and _B.
-        __m256i left    = _mm256_loadu_si256((__m256i* const)aStr);
-        __m256i right   = _mm256_loadu_si256((__m256i* const)bStr);
+        const __m256i left    = _mm256_loadu_si256((__m256i* const)aStr);
+        const __m256i right   = _mm256_loadu_si256((__m256i* const)bStr);
 
-        __m256i res_and = _mm256_and_si256(left, right);
-        __m256i cmp     = _mm256_cmpeq_epi8(res_and, left);
+        const __m256i res_and = _mm256_and_si256(left, right);
+        const __m256i cmp     = _mm256_cmpeq_epi8(res_and, left);
 
         if (_mm256_movemask_epi8(cmp) != 0xffffffff)
             return false;
@@ -59,11 +61,11 @@ static SONICSTR_INLINE bool simd_str_cmp( const char* aStr, const char* bStr, si
     while (aLen > 16)
     {
         // Works like AVX but with 128 bits instead of 256.
-        __m128i left    = _mm_loadu_epi8((const void*)aStr);
-        __m128i right   = _mm_loadu_epi8((const void*)bStr);
+        const __m128i left    = _mm_loadu_epi8((const void*)aStr);
+        const __m128i right   = _mm_loadu_epi8((const void*)bStr);
 
-        __m128i res_and = _mm_and_si128(left, right);
-        __m128i cmp     = _mm_cmpeq_epi8(res_and, left);
+        const __m128i res_and = _mm_and_si128(left, right);
+        const __m128i cmp     = _mm_cmpeq_epi8(res_and, left);
 
         if (_mm_movemask_epi8(cmp) != 0xFFFF)
             return false;
@@ -98,8 +100,8 @@ static SONICSTR_INLINE bool simd_str_cmp( const char* aStr, const char* bStr, si
     while(aLen > 4)
     {
         // Do the same as above but cast to integer and process in groups of 4 bytes.
-        const unsigned int left    = *((unsigned int* const)aStr);
-        const unsigned int right   = *((unsigned int* const)bStr);
+        const unsigned int left    = *((const unsigned int* const)aStr);
+        const unsigned int right   = *((const unsigned int* const)bStr);
 
         if(left != right)
             return false;
@@ -124,11 +126,118 @@ static SONICSTR_INLINE bool simd_str_cmp( const char* aStr, const char* bStr, si
     return true;
 }
 
+
+template <typename T>
+static SONICSTR_INLINE SONICSTR_CONSTEXPR T clear_leftmost_set(const T value) SONICSTR_NOEXCEPT
+{
+    return value & (value - 1);
+}
+
+
+template <typename T>
+static SONICSTR_INLINE SONICSTR_CONSTEXPR unsigned get_first_bit_set(const T value) SONICSTR_NOEXCEPT
+{
+    return std::countr_zero(value);
+}
+
+// If we have AVX2 Instruction set defined, Sonic str wwill default
+// to avx algo. If we cannot find AVX2 instruction set, we will go with
+// SWAR method. 
+// Returns either index into haystack of needle if found. 
+// or
+// -1 as size_t or largest possible size_t if not found.
+
+// Based on:
+// http://0x80.pl/articles/simd-strfind.html
+static SONICSTR_INLINE SONICSTR_CONSTEXPR size_t simd_swar_str_contains_needle( const char* str, size_t len, const char* needle, size_t needle_len ) SONICSTR_NOEXCEPT
+{
+#ifdef __AVX2__
+
+    // Vectors holding first and last characters of needle
+    const __m256i needle_first_char_v = _mm256_set1_epi8( needle[0] );              // At begin - 0
+    const __m256i needle_last_char_v  = _mm256_set1_epi8( needle[needle_len - 1]);  // At end   - Len - 1
+    
+    // Iterate in blocks of 32.
+    for(size_t i = 0; i < len; i += 32)
+    {
+        // Extract chunks
+        const __m256i str_block_first = _mm256_loadu_si256( (const __m256i*) str + i  );
+        const __m256i str_block_last  = _mm256_loadu_si256( (const __m256i*) str + i + needle_len - 1);
+    
+        // Compare extracted chunks with our character vectors
+        // holdin the first and last letters of the needle.
+        const __m256i equality_first = _mm256_cmpeq_epi8( needle_first_char_v, str_block_first );
+        const __m256i equality_last  = _mm256_cmpeq_epi8( needle_last_char_v, str_block_last );
+
+        // With this mask we extract the positions where we match with out needles first/last characters.    
+        unsigned int mask = _m256_movemask_epi8( _mm256_and_si256( equality_first, equality_last ) );
+    
+        // While mask contains any set bits it means there are still possible locations
+        // where we can find the needle.
+        while(mask != 0)
+        {
+            // Extract index or position from the first found set bit.
+            const auto bit_pos = ::Sonic::get_first_bit_set( mask );
+            
+            // If needle maatches we have found it inside the chunk.
+            if( memcmp( s + i + bit_pos + 1, needle + 1, needle_len - 2 ) == 0)
+                return i + bit_pos;
+        
+            // If not, we clear given flagged position removing its set state.
+             mask = ::Sonic::clear_leftmost_set(mask);
+        }
+    }
+
+    // If we reach this, it means needle was not found :(
+    // Highest possible size_t value.
+    return -1;
+#else
+    // If we cant find AVX instruction set, fallback to SWAR for now...
+    
+    // Construct "vectors" containing the first and last characters of our needle.
+    const unsigned long long needle_first_char_v = 0x0101010101010101llu * (unsigned char)needle[0];
+    const unsigned long long needle_last_char_v  = 0x0101010101010101llu * (unsigned char)needle[ needle_len - 1 ];
+
+    unsigned long long* str_block_first = (unsigned long long*)(str);
+    unsigned long long* str_block_last  = (unsigned long long*)(str + needle_len - 1);    
+
+    // Iterate in 8 byte (64 bit) blocks/chunks.
+    for(size_t i = 0; i < len; i += 8, str_block_first++, str_block_last++)
+    {
+    
+        const unsigned long long equality = (*str_block_first ^ needle_first_char_v) | ( *str_block_last ^ needle_last_char_v );
+    
+        const unsigned long long t0 = (~equality & 0x7f7f7f7f7f7f7f7fllu) + 0x0101010101010101llu;
+        const unsigned long long t1 = (~equality & 0x8080808080808080llu);
+    
+        unsigned long long zeros = t0 & t1;
+        size_t j = 0;
+        
+        while( zeros )
+        {
+            if(zeros & 0x80)
+            {
+                const char* sub_str = (char*)str_block_first + j + 1;
+                if(memcmp( sub_str, needle + 1, needle_len - 2 ) == 0)
+                    return i + j;
+            }
+            
+            zeros   >>= 8;
+            j       += 1;
+        }
+    }
+
+    // Highest possible size_t value.
+    return -1;
+#endif
+}
+
+
 // We dont do any sanity checks. We dont check if any
 // of the forwarded strings are NULL. We also expect
 // both strings to be the same length...
 // Careful!!!! DANGEROUS!
-static SONICSTR_INLINE bool str_cmp( const char* aStr, const char* bStr, size_t aLen ) SONICSTR_NOEXCEPT
+static SONICSTR_INLINE SONICSTR_CONSTEXPR bool str_cmp( const char* aStr, const char* bStr, size_t aLen ) SONICSTR_NOEXCEPT
 {
     // Compare first character, if different simply ignore
     // and move on....
@@ -137,19 +246,40 @@ static SONICSTR_INLINE bool str_cmp( const char* aStr, const char* bStr, size_t 
         
     return simd_str_cmp( aStr, bStr, aLen );
 }
+
+// Looks for needle of len ´needle_len´ in string of len ´len´.
+// If needle is found, will return pointer to provided string
+// at begin of found needle, else will return nullptr.
+static SONICSTR_INLINE SONICSTR_CONSTEXPR char* str_str( const char* str, size_t len, const char* needle, size_t needle_len ) SONICSTR_NOEXCEPT
+{
+    // Call to find needle. using underlying Sonic function.
+    size_t idx = ::Sonic::simd_swar_str_contains_needle( str, len, needle, needle_len );
+
+    // If needle is found, return pointer to string at found needle position.
+    // If needle is not found, return nullptr.
+    return (idx != -1) ? (char*)(str + idx) : nullptr;
+}
+
+
+static SONICSTR_INLINE SONICSTR_CONSTEXPR char* str_str_s( const char* str, size_t len, const char* needle, size_t needle_len ) SONICSTR_NOEXCEPT
+{
+    // Sanity checks for safe str str version.
+    if(!str || !needle || ( needle_len > len ) || str == needle)
+        return nullptr;
+
+    // Use underlying STR STR func.
+    return ::Sonic::str_str( str, len, needle, needle_len );
+}
+
 // Safe sanity check version.
-static SONICSTR_INLINE bool str_cmp_s( const char* aStr, const char* bStr, size_t aLen ) SONICSTR_NOEXCEPT
+static SONICSTR_INLINE SONICSTR_CONSTEXPR bool str_cmp_s( const char* aStr, const char* bStr, size_t aLen ) SONICSTR_NOEXCEPT
 {
     // Sanity checks.
     if(!aStr || !bStr)
         return false;
-
-    // Compare first character, if different simply ignore
-    // and move on....
-    if(*aStr != *bStr)
-        return false;
-        
-    return simd_str_cmp( aStr, bStr, aLen );
+    
+    // I hope the compiler inlines this <3
+    return ::Sonic::str_cmp( aStr, bStr, aLen );
 }
 
 }
---
+++ sample.cpp
@@ -0,0 +1,29 @@
+#include "SonicStr.h"
+#include <stdio.h>
+
+inline void print_bool( bool state )
+{
+    printf("%s\n", state ? "true" : "false" );
+} 
+
+int main(int argc, char** argv)
+{
+
+    const char* a = "aaaaa";
+    const char* b = "bbbbb";
+
+    size_t alen = strlen( a );
+
+    const char *needle = " for";
+    const char *haystack = "This is a test haystack, it is longer than the needle for obv reasons!";
+    
+    size_t needle_len = strlen( needle );
+    size_t haystack_len = strlen( haystack );
+
+    volatile bool res = Sonic::str_cmp( a, b, alen );  
+    print_bool( res );
+    
+    print_bool(Sonic::str_str(haystack, haystack_len, needle, needle_len ));
+    
+    return 0;
+}
